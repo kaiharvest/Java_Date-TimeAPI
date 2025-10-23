00416c5c83564c8b2c00922a5c2a075db3d02a06
kaiharvest <indradwiprabowo27@gmail.com>
2025-10-23 22:12:29 +0700

    Time Zone

---
+++ src/test/java/programmer_zaman_now/belajar_java_datetime/TimeZoneTest.java
@@ -0,0 +1,40 @@
+package programmer_zaman_now.belajar_java_datetime;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class TimeZoneTest {
+
+    @Test
+    void testTimeZone() {
+        TimeZone timeZoneDefault = TimeZone.getDefault();
+        System.out.println(timeZoneDefault);
+
+        TimeZone timeZoneGMT = TimeZone.getTimeZone("GMT");
+        System.out.println(timeZoneGMT);
+
+        String[] availableIDs = TimeZone.getAvailableIDs();
+        Arrays.asList(availableIDs).forEach(System.out::println);
+    }
+
+    @Test
+    void testDate() {
+        Date date = new Date();
+        System.out.println(date.toString());
+        System.out.println(date.toGMTString());
+    }
+
+    @Test
+    void calendar() {
+        Calendar calendarJakarta = Calendar.getInstance(TimeZone.getTimeZone("Asia/Jakarta"));
+        Calendar calendarGMT = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+
+        System.out.println(calendarJakarta.get(Calendar.HOUR_OF_DAY));
+        System.out.println(calendarGMT.get(Calendar.HOUR_OF_DAY));
+    }
+}
