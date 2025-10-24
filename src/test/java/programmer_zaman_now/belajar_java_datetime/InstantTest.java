9844fb0b44e440a171925ae8ed6c9dec4beed713
kaiharvest <indradwiprabowo27@gmail.com>
2025-10-24 23:10:11 +0700

    Instant

--- src/test/java/programmer_zaman_now/belajar_java_datetime/InstantTest.java
+++ src/test/java/programmer_zaman_now/belajar_java_datetime/InstantTest.java
@@ -2,7 +2,7 @@ package programmer_zaman_now.belajar_java_datetime;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.Instant;
+import java.time.*;
 
 public class InstantTest {
 
@@ -13,5 +13,61 @@ public class InstantTest {
 
         Instant instant1 = Instant.ofEpochMilli(0);
         System.out.println(instant1);
+
+        Instant instant2 = Instant.ofEpochMilli(0);
+        System.out.println(instant2);
+    }
+
+    @Test
+    void modify() {
+        Instant instant1 = Instant.now();
+        Instant instant2 = instant1.plusMillis(1000);
+        Instant instant3 = instant1.minusMillis(1000);
+
+        System.out.println(instant1);
+        System.out.println(instant2);
+        System.out.println(instant3);
+
+    }
+
+    @Test
+    void get() {
+        Instant instant = Instant.now();
+
+        System.out.println(instant.toEpochMilli());
+        System.out.println(instant.getEpochSecond());
+        System.out.println(instant.getNano());
+    }
+
+    @Test
+    void fromInstant() {
+        Instant instant = Instant.now();
+
+        ZoneId zoneId = ZoneId.of("Asia/Jakarta");
+
+        LocalTime localTime = LocalTime.ofInstant(instant, zoneId);
+        System.out.println(localTime);
+
+        LocalDateTime loacalDateTime = LocalDateTime.ofInstant(instant, zoneId);
+        System.out.println(loacalDateTime);
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(instant, zoneId);
+        System.out.println(zonedDateTime);
+
+        OffsetTime offsetTime = OffsetTime.ofInstant(instant, zoneId);
+        System.out.println(offsetTime);
+
+    }
+
+    @Test
+    void toInstant() {
+        Instant instant1 = LocalDateTime.now().toInstant(ZoneOffset.ofHours(7));
+        System.out.println(instant1);
+
+        Instant instant2 = ZonedDateTime.now().toInstant();
+        System.out.println(instant2);
+
+        Instant instant3 = OffsetDateTime.now().toInstant();
+        System.out.println(instant3);
     }
 }
